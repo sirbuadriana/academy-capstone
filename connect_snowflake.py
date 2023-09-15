5e97c3f352c4ac2561c9e5ef41a2d8e53b988551
Sirbu Adriana <sirbuadriana@yahoo.com>
2023-09-15 08:14:40 +0000

    added new files

---
+++ connect_snowflake.py
@@ -0,0 +1,9 @@
+import botocore 
+import botocore.session 
+from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
+
+client = botocore.session.get_session().create_client('secretsmanager')
+cache_config = SecretCacheConfig()
+cache = SecretCache( config = cache_config, client = client)
+
+secret = cache.get_secret_string('app/ingest.py')
