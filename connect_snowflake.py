c7ccce6fdf68bb9c44fccde8040f4f4fce4ed18a
Sirbu Adriana <sirbuadriana@yahoo.com>
2023-09-15 08:24:25 +0000

    can get the secret for snow

--- connect_snowflake.py
+++ connect_snowflake.py
@@ -6,4 +6,6 @@ client = botocore.session.get_session().create_client('secretsmanager')
 cache_config = SecretCacheConfig()
 cache = SecretCache( config = cache_config, client = client)
 
-secret = cache.get_secret_string('app/ingest.py')
+secret = cache.get_secret_string('snowflake/capstone/login')
+
+print(secret)
