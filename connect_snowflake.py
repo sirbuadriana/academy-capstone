8e8cbe780b43613018c56d3912d7ae1edbd58afb
Sirbu Adriana <sirbuadriana@yahoo.com>
2023-09-15 08:30:07 +0000

    can get the secret for snow modified

--- connect_snowflake.py
+++ connect_snowflake.py
@@ -1,11 +1,18 @@
 import botocore 
 import botocore.session 
+import json
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
 
-client = botocore.session.get_session().create_client('secretsmanager')
-cache_config = SecretCacheConfig()
-cache = SecretCache( config = cache_config, client = client)
+def get_snowflake_credentials():
+    client = botocore.session.get_session().create_client('secretsmanager')
+    cache_config = SecretCacheConfig()
+    cache = SecretCache( config = cache_config, client = client)
+    secret = cache.get_secret_string('snowflake/capstone/login')
+    return json.loads(secret)
 
-secret = cache.get_secret_string('snowflake/capstone/login')
 
-print(secret)
+
+if __name__ == "__main__":
+    secret = get_snowflake_credentials()
+    print(secret["DATABASE"])
+    print(secret["PASSWORD"])
